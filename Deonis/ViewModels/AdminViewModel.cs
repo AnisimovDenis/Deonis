27f60653eaf9d3f5c69f3d8752dc2beae0f82873
Денис Анисимов <den.anisimov.94@mail.ru>
2020-05-23 15:01:36 +0300

    Добавил библиотеку MVVMLight

---
+++ Deonis/ViewModels/AdminViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Deonis_lib.MVVM;
+
+namespace Deonis.ViewModels
+{
+    internal class AdminViewModel
+    { 
+
+    }
+}
